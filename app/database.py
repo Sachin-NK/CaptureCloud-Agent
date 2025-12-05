90d6b3e0894aa7614f0fd723761a9b8492de0e39
Sachin-NK <snkodikara52@gmail.com>
2025-12-05 10:57:12 +0530

    adding database Connection Manager

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from supabase import create_client, Client
+from app.config import get_settings
+
+settings = get_settings()
+
+# Regular Supabase client - uses the public anon key
+supabse:Client = create_client(settings.supabase_url, settings.supabase_key)
+
+# Agent Supabase client 
+supabase_agent: Client = create_client(settings.supabase_url, settings.supabase_agent_key)
+
+def get_supabase() -> Client:
+    return supabse
+
+def get_supabase_agent() -> Client:
+    return supabase_agent
+
+def set_user_context(db: Client, access_token: str, refresh_token: Optional[str] = None):
+    
+    db.auth.set_session({
+        "access_token": access_token,
+        "refresh_token": refresh_token
+    })
+
+def clear_user_context(db: Client):
+    db.auth.sign_out()
